91d2ee62612f0f4c4ca835e7ada8bebd03418443
Abhay <tabhay373@gmail.com>
2026-03-22 00:10:28 +0530

    fix langchain text splitter import

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ from pydantic import BaseModel
 from langchain_core.runnables import RunnableParallel, RunnablePassthrough, RunnableLambda
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import PromptTemplate
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_groq import ChatGroq
